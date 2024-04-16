8e7292089dc4d19b88b90a6791984bebb828b292
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2024-04-16 11:48:03 +0200

    Formatting, test update, renaming

--- tests/test_updated_esdl_post_process.py
+++ tests/test_updated_esdl_post_process.py
@@ -1,3 +1,4 @@
+import fnmatch
 import os
 import sys
 from pathlib import Path
@@ -19,13 +20,13 @@ class TestUpdatedESDL(TestCase):
         Checks:
         - That the correct number of KPIs have been added
         - That the correct assets have been removed
-        - Check that all the assets have a state=ENABLED
-
-        Add:
-        - pipe diam
-        - aggregation count of the asset -> check in mesido vs esdl
-        - units of the kips euro
-        - if the assets are connected
+        - That all the assets have a state=ENABLED
+        - The diameter of all the pipes are as expected
+        - The aggregation count of the assets, MESIDO problem vs updated ESDL file
+        - That the KPI values are represented in the correct units
+        - That assets are connected and that the connections per ports were not changed in the
+          updated ESDL
+        - The correct number of polygon sub-areas exist
         """
 
         root_folder = str(Path(__file__).resolve().parent.parent)
@@ -46,6 +47,7 @@ class TestUpdatedESDL(TestCase):
             model_folder=model_folder,
             input_folder=input_folder,
         )
+        problem.pre()
 
         # test KPIs in optimized ESDL
         esdl_path = os.path.join(base_folder, "model", "PoC Tutorial_GrowOptimized.esdl")
@@ -53,36 +55,60 @@ class TestUpdatedESDL(TestCase):
 
         # High level checks of KPIs
         number_of_kpis_top_level_in_esdl = 8
-        high_level_kpis_name = [
+        high_level_kpis_euro = [
             "High level cost breakdown [EUR]",
             "Overall cost breakdown [EUR]",
             "CAPEX breakdown [EUR]",
             "OPEX breakdown [EUR]",
-            "Energy production [Wh]",
             "Area_76a7: Asset cost breakdown [EUR]",
             "Area_9d0f: Asset cost breakdown [EUR]",
             "Area_a58a: Asset cost breakdown [EUR]",
         ]
+        high_level_kpis_wh = [
+            "Energy production [Wh]",
+        ]
+        all_high_level_kpis = []
+        all_high_level_kpis = high_level_kpis_euro + high_level_kpis_wh
+
         np.testing.assert_allclose(
             len(optimized_energy_system.instance[0].area.KPIs.kpi), number_of_kpis_top_level_in_esdl
         )
         for ii in range(len(optimized_energy_system.instance[0].area.KPIs.kpi)):
             kpi_name = optimized_energy_system.instance[0].area.KPIs.kpi[ii].name
             np.testing.assert_array_equal(
-                kpi_name in high_level_kpis_name,
+                kpi_name in all_high_level_kpis,
                 True,
                 err_msg=f"KPI name {kpi_name} was not expected in the ESDL",
             )
+            if kpi_name in high_level_kpis_euro:
+                np.testing.assert_array_equal(
+                    optimized_energy_system.instance[0].area.KPIs.kpi[ii].quantityAndUnit.unit.name,
+                    "EURO",
+                )
+            elif kpi_name in high_level_kpis_wh:
+                np.testing.assert_array_equal(
+                    optimized_energy_system.instance[0].area.KPIs.kpi[ii].quantityAndUnit.unit.name,
+                    "WATTHOUR",
+                )
+            else:
+                exit(f"Unexpected KPI name: {kpi_name}")
 
-        # High level checks of the assets
-        # Check quantity of assets and that they all of have an ENABLED state
+        # Check the asset quantity
         number_of_assets_in_esdl = 15
         np.testing.assert_allclose(
             len(optimized_energy_system.instance[0].area.asset), number_of_assets_in_esdl
         )
+        # Check:
+        # - that the correct assets were removed
+        # - asset state
+        # - pipe diameter sizes
+        # - asset aggregation count
+        # - number of ports
+        # - number of connection to a port
         asset_to_be_deleted = ["ResidualHeatSource_76f0", "Pipe_8fa5_ret", "Pipe_8fa5"]
         for ii in range(len(optimized_energy_system.instance[0].area.asset)):
             asset_name = optimized_energy_system.instance[0].area.asset[ii].name
+            # Existance of asset and its state
             np.testing.assert_array_equal(
                 asset_name not in asset_to_be_deleted,
                 True,
@@ -91,6 +117,48 @@ class TestUpdatedESDL(TestCase):
             np.testing.assert_array_equal(
                 optimized_energy_system.instance[0].area.asset[ii].state.name == "ENABLED", True
             )
+
+            # Check pipe diameter
+            if (
+                len(
+                    fnmatch.filter([optimized_energy_system.instance[0].area.asset[ii].id], "Pipe*")
+                )
+                == 1
+            ):
+                if asset_name not in ["Pipe4", "Pipe4_ret", "Pipe5", "Pipe5_ret"]:
+                    np.testing.assert_array_equal(
+                        optimized_energy_system.instance[0].area.asset[ii].diameter.name, "DN400"
+                    )
+                else:
+                    np.testing.assert_array_equal(
+                        optimized_energy_system.instance[0].area.asset[ii].diameter.name,
+                        "DN300",
+                        err_msg=f"Asset name {asset_name} was not expected in the ESDL",
+                    )
+                # Check aggregation count
+                np.testing.assert_array_equal(
+                    optimized_energy_system.instance[0].area.asset[ii].aggregationCount,
+                    problem.get_aggregation_count_max(asset_name),
+                )
+                # Check the number of ports of the assets are as expected
+                np.testing.assert_array_equal(
+                    len(optimized_energy_system.instance[0].area.asset[ii].port),
+                    len(problem.esdl_assets[asset_name].in_ports)
+                    + len(problem.esdl_assets[asset_name].out_ports),
+                )
+                # Check the number of connection to a port
+                optimized_energy_system.instance[0].area.asset[ii].port[1].name
+                for iport in range(len(optimized_energy_system.instance[0].area.asset[ii].port)):
+                    if optimized_energy_system.instance[0].area.asset[ii].port[iport].name == "In":
+                        np.testing.assert_array_equal(
+                            len(
+                                optimized_energy_system.instance[0]
+                                .area.asset[ii]
+                                .port[iport]
+                                .connectedTo.items
+                            ),
+                            len(problem.esdl_assets[asset_name].in_ports),
+                        )
 
         # High level check on the polygon areas drawn
         number_of_areas_in_esdl = 3
