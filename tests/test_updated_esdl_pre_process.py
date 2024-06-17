a4a2b09cf7b2e8d16d3bf0e4c0b2ca5662c7d73c
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2024-06-17 11:32:57 +0200

    Update test case to include checking that the correct profile is assigned per demand

--- tests/test_updated_esdl_pre_process.py
+++ tests/test_updated_esdl_pre_process.py
@@ -5,15 +5,20 @@ from unittest import TestCase
 from mesido.esdl.esdl_parser import ESDLFileParser
 from mesido.workflows import run_end_scenario_sizing
 
+import numpy as np
+
 
 class TestUpdatedESDL(TestCase):
-
     def test_updated_esdl(self):
         """
         Check that the updated ESDL resulting from optmizing a network, is correct by using the
         PoCTutorial and the Grow_workflow. This is done for the actual esdl file and the esdl
         string created by MESIDO. Both these resulting optimized energy systems should be identical
         and it is only the MESIDO esdl saving method that differs.
+
+        Additional checks:
+         - Check that the unique profile identification in profile_parser assigns the correct
+         profile
 
         """
 
@@ -41,6 +46,35 @@ class TestUpdatedESDL(TestCase):
         )
         file.write(optimized_esdl_string)
         file.close()
+
+        # Check the unique profile identification in profile_parser
+        # Test that the correct demand profile is assigned to a demand as expected. Note that the
+        # demand profiles are the adapted profiles (peak-hourly, rest-5daily). Therefore the
+        # expected max and average hard-coded values are compared to the problem values.
+        np.testing.assert_allclose(
+            1201200.0,  # demand4_MW, multiplier 0.75
+            max(problem.get_timeseries("HeatingDemand_b0ff.target_heat_demand").values),
+        )
+        np.testing.assert_allclose(
+            720720.0,  # demand5_MW, multiplier 0.3
+            max(problem.get_timeseries("HeatingDemand_8fbe.target_heat_demand").values),
+        )
+        np.testing.assert_allclose(
+            916316.712,  # demand1_MW, multiplier 0.5
+            max(problem.get_timeseries("HeatingDemand_08fd.target_heat_demand").values),
+        )
+        np.testing.assert_allclose(
+            443413.79,  # demand4_MW, multiplier 0.75
+            np.average(problem.get_timeseries("HeatingDemand_b0ff.target_heat_demand").values),
+        )
+        np.testing.assert_allclose(
+            266048.27,  # demand5_MW, multiplier 0.3
+            np.average(problem.get_timeseries("HeatingDemand_8fbe.target_heat_demand").values),
+        )
+        np.testing.assert_allclose(
+            575570.74,  # demand1_MW, multiplier 0.5
+            np.average(problem.get_timeseries("HeatingDemand_08fd.target_heat_demand").values),
+        )
 
 
 if __name__ == "__main__":
