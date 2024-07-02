3a7aefb14ca183952ae9e69f613d2b5c02554f37
kobus.vanrooyen@tno.nl <kobus.vanrooyen@tno.nl>
2024-07-02 13:25:58 +0200

    remove all temp code, use correct code

--- src/mesido/_darcy_weisbach.py
+++ src/mesido/_darcy_weisbach.py
@@ -2,8 +2,6 @@ import math
 
 import CoolProp as cP
 
-from iapws import IAPWS95
-
 from mesido.constants import GRAVITATIONAL_CONSTANT
 from mesido.network_common import NetworkSettings
 
@@ -23,19 +21,9 @@ def _kinematic_viscosity(temperature, network_type=NetworkSettings.NETWORK_TYPE_
 
     # Kinematic viscosity [m2/s] = Dynamic viscosity [Pa-s] / density [kg/m3]
     if network_type == NetworkSettings.NETWORK_TYPE_HEAT:
-        old = IAPWS95(T=273.15 + temperature, P=0.5).nu
-        new = cP.CoolProp.PropsSI(
-            "V", "T", 273.15 + temperature, "P", 0.5 * 10**6, "WATER"
-        ) / cP.CoolProp.PropsSI("D", "T", 273.15 + temperature, "P", 0.5 * 10**6, "WATER")
-
-        if abs(old - new) / old * 100.0 > 0.001:
-            exit(1)
-        
-        # return IAPWS95(T=273.15 + temperature, P=0.5).nu
         return cP.CoolProp.PropsSI(
             "V", "T", 273.15 + temperature, "P", 0.5 * 10**6, "WATER"
         ) / cP.CoolProp.PropsSI("D", "T", 273.15 + temperature, "P", 0.5 * 10**6, "WATER")
-
     elif network_type == NetworkSettings.NETWORK_TYPE_HYDROGEN:
         pressure = pressure if pressure else 101325
         return cP.CoolProp.PropsSI(
